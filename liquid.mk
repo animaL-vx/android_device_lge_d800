a0d925b15ea4bad9171e56d6b6378f09fd0aa005
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-16 08:31:51 -0600

    Add nfc and update layout

--- liquid.mk
+++ liquid.mk
@@ -1,23 +1,51 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Boot animation
+# boot
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common liquid stuff.
+# device
+$(call inherit-product, device/lge/d800/d800.mk)
+
+# phone
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/lge/d800/d800.mk)
+# telephony
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := liquid_d800
 PRODUCT_BRAND := LGE
+PRODUCT_NAME := liquid_d800
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.d800.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_att_us/g2:4.2.2/JDQ39B/D80010d.1376460177:user/release-keys PRIVATE_BUILD_DESC="g2_att_us-user 4.2.2 JDQ39B D80010d.1376460177 release-keys"
+# override
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=D80010d.1376460177 \
+    PRODUCT_NAME=d801 \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="g2_att_us-user 4.2.2 JDQ39B D80010d.1376460177 release-keys" \
+    BUILD_FINGERPRINT="lge/g2_att_us/g2:4.2.2/JDQ39B/D80010d.1376460177:user/release-keys"
 
 PRODUCT_PACKAGES += Torch
