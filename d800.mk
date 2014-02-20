11b97369852b296521773a207cd9ec707af48d6c
Sean Hoyt <seanhoyt963@gmail.com>
2014-02-20 14:59:22 -0600

    Those apns our not needed

--- d800.mk
+++ d800.mk
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
